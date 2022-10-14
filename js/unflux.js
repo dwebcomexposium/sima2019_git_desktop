2d9913a81c53cb450c220bd08d28289c95137595
Aurélien Ravier <aurelien.ravier@unflux.fr>
2022-10-14 11:35:01 +0200

    Update unflux.js

--- js/unflux.js
+++ js/unflux.js
@@ -1,6 +1,27 @@
-(function($) {
+const setCookie = (cname, cvalue, exdays) => {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
-    console.log('maj')
+const getCookie = (cname) => {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+(function($) {
     var simaJS = {
 
         init : function(){
@@ -207,6 +228,8 @@
                     popUp.style.display = 'none';
                     HTMLElement.classList.remove('noscroll')
                 })
+                
+                setCookie('popup', 'true', 10)
             }
         }
 
