7f112aea58bf6adf0b8b07c99a1d54cecf8fa2d4
Aurélien Ravier <aurelien.ravier@unflux.fr>
2022-12-09 10:30:25 +0100

    Update unflux.js

--- js/unflux.js
+++ js/unflux.js
@@ -187,7 +187,7 @@ const getCookie = (cname) => {
                 dateSection.innerHTML = `
                 <div class="salon-dates__start salon-dates__part">
                     <p class="salon-dates__day">24</p>
-                    <p class="salon-dates__month">${lang == "fr" ? 'novembre' ? 'november'}</p>
+                    <p class="salon-dates__month">${lang == "fr" ? 'novembre' : 'november'}</p>
                     <p class="salon-dates__year">2024</p>
                     <svg viewBox="0 0 46 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6 11.3333C8.94552 11.3333 11.3333 8.94552 11.3333 6C11.3333 3.05448 8.94552 0.666667 6 0.666667C3.05448 0.666667 0.666667 3.05448 0.666667 6C0.666667 8.94552 3.05448 11.3333 6 11.3333ZM6 7L46 7V5L6 5V7Z" fill="#252525"/>
